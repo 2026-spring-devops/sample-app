e109a2dbda4866b2998eb542a572954ebbaaa09c
nataliekufel <nataliekufel1@gmail.com>
2026-01-29 12:14:47 -0600

    Finished week2 assignment

---
+++ app.js
@@ -0,0 +1,29 @@
+const http = require(
+    'http'
+);
+
+const server = http.createServer(
+    (req, res) => {
+        res.writeHead(
+            200, 
+            {
+                'Content-Type': 'text/plain' 
+            }
+        );
+        res.end(
+            `Hello, Natalie's World!
+The time on this server is: ${new Date().toLocaleString()}
+The D6 rolled a ${Math.floor(Math.random() * 6) + 1}!
+The D20 rolled a ${Math.floor(Math.random() * 20) + 1}!`
+        );
+    }
+);
+
+const port = process.env.PORT || 8080;
+
+server.listen(
+    port, 
+    () => {
+        console.log(`Listening on port ${port}`);
+    }
+);
