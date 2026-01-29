ec284d43197a9db1b7985fc26128b4523cfb0236
rvanovermeiren-matc <rvanovermeiren@madisoncollege.edu>
2026-01-29 11:45:01 -0600

    Added server time to app

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, Rane\'s World ! ! !\n');
-});
+  res.end(`
+    Hello, Rane\'s World ! ! !
+    
+    The time on the server is ${new Date().toLocaleString()}
+`
+        );
+    }
+);
 
 const port = process.env.PORT || 8080;
 server.listen(port,() => {
