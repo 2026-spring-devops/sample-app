7532482177af78ddd25f672c157f150d8eda04dd
jarengomatc <jarengo@madisoncollege.edu>
2026-01-29 11:44:14 -0600

    Added server time to output...

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end("Hello, John's World ! ! !\n");          
+  res.end(`Hello, John's World ! ! !
+
+The time on the server is ${new Date().toLocaleString()}`);   
 });
 
 const port = process.env.PORT || 8080; 
