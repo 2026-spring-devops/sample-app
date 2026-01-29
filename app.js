0809fd23c85e556ac511911e5127f480b352fe41
Peter Barbiaux <PBarbiaux@madisoncollege.edu>
2026-01-29 11:59:40 -0600

    added another roll, a d14, due to Europa Universalis

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end(`Hello, Peter's World! $!$!$!\n
     The current time on the server is: ${new Date().toLocaleString()}
-    The d20 rolled a ${Math.floor(Math.random() * 20) + 1}`);          
+    The d20 rolled a ${Math.floor(Math.random() * 20) + 1}
+    the d14 rolled a ${Math.floor(Math.random() * 14) + 1}`);          
 });
 
 const port = process.env.PORT || 8080; 
