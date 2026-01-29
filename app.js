2682e22eb4120437df8b3f9e1f300c2d3d94cdfc
Jeffrey Hildebrand <jphildebrand@madisoncollege.edu>
2026-01-29 11:23:32 -0600

    got book version of app js running. Also saw we need to relaunch file.

---
+++ app.js
@@ -0,0 +1,24 @@
+const http = require(
+    'http'
+);
+
+const server = http.createServer(
+    (req, res) => {
+  res.writeHead(
+    200,
+     { 'Content-Type': 'text/plain'
+
+      });
+  res.end(
+    "Hello, Jeff's World!!!!\n"
+);          
+});
+
+const port = process.env.PORT || 8080;
+
+server.listen(
+    port,
+    () => {
+  console.log(`Listening on port ${port}`);
+    }
+);
