6dca9e0d424a101eed8941cc06c2d3849dbb2f39
jarengomatc <jarengo@madisoncollege.edu>
2026-01-29 11:53:53 -0600

    Add random D6

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end(`Hello, John's World ! ! !
 
-The time on the server is ${new Date().toLocaleString()}`);   
+The time on the server is ${new Date().toLocaleString()}
+
+The D6 rolled a ${Math.floor(Math.random() * 6) + 1}`);
 });
 
 const port = process.env.PORT || 8080; 
