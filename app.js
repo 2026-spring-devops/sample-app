b9f7aebcb994e29e02449cf9e00d894f39fa523e
Peter Barbiaux <PBarbiaux@madisoncollege.edu>
2026-01-29 11:53:28 -0600

    added a d20 roll to the server response

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end(`Hello, Peter's World! $!$!$!\n
-    The current time on the server is: ${new Date().toLocaleString()}`);          
+    The current time on the server is: ${new Date().toLocaleString()}
+    The d20 rolled a ${Math.floor(Math.random() * 20) + 1}`);          
 });
 
 const port = process.env.PORT || 8080; 
