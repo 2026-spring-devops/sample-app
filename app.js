089f68b497678ceff4aa8b63a67021e6e5956211
jarengomatc <jarengo@madisoncollege.edu>
2026-01-29 11:57:50 -0600

    Add D20 roll output to server response

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const server = http.createServer((req, res) => {
 
 The time on the server is ${new Date().toLocaleString()}
 
-The D6 rolled a ${Math.floor(Math.random() * 6) + 1}`);
+The D6 rolled a ${Math.floor(Math.random() * 6) + 1}
+
+The D20 rolled a ${Math.floor(Math.random() * 20) + 1}`);
 });
 
 const port = process.env.PORT || 8080; 
