357f69fe007035b5855fadd170e54ac977993558
Peter Barbiaux <PBarbiaux@madisoncollege.edu>
2026-02-03 11:07:21 -0600

    added server hostname lookup to output instead of hardcoding it as "the server"

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
+const os = require('os');
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end(`Hello, Peter's World! $!$!$!\n
-    The current time on the server is: ${new Date().toLocaleString()}
+    The current time on ${os.hostname} is: ${new Date().toLocaleString()}
     The d20 rolled a ${Math.floor(Math.random() * 20) + 1}
     the d14 rolled a ${Math.floor(Math.random() * 14) + 1}`);          
 });
