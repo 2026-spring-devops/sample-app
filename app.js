036505dd724587d7dd998a0c62c0a17d2789fec5
bpaulisse <bpaulisse@madisoncollege.edu>
2026-02-03 11:07:33 -0600

    Added hostname to the output

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const http = require('http');
+const os = require('os');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end(
 `Hello, Brock's World!
+Hostname: ${os.hostname()}
 Server Time: ${new Date().toLocaleString()}
 The D6 rolled a ${Math.floor( Math.random() * 6 ) + 1}
 The D20 rolled a ${Math.floor( Math.random() * 20 ) + 1}`
