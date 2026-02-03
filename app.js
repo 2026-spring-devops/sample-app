d2a113c5ca8c824cf4f4892e7e0a6d8e7b87265c
rvanovermeiren-matc <rvanovermeiren@madisoncollege.edu>
2026-02-03 11:39:58 -0600

    added D17 for 3rd roll

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ The time on ${os.hostname()} (server) is ${new Date().toLocaleString()}
 
 The D6 rolled a ${Math.floor(Math.random() * 6) + 1}
 The D20 rolled a ${Math.floor(Math.random() * 20) + 1}
+The D17 rolled a ${Math.floor(Math.random() * 17) + 1}
 `
         );
     }
