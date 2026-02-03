08e87c76ea79aa3a28e56a06512cdc44335de2d9
deecolg <drieberutlin@madisoncollege.edu>
2026-02-03 11:33:01 -0600

    Added .env file also added line 4-7 in app.js

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ const http = require(
     'http'
 );
 
+const os = require(
+    'os'
+);
+
 const server = http.createServer(
     (req, res) => {
         res.writeHead(
             200, 
-            { 
-                'Content-Type': 'text/plain' 
-            }
-        );
-        res.end(`
-            Hello, Devons World! 
-            The time on the server is ${new Date().toLocaleString()}
+                { 
+                    'Content-Type': 'text/plain' 
+                }
+            );
+        res.end(`Hello, Thomas' World ! ! !
+
+The time on ${os.hostname()} (server) is ${new Date().toLocaleString()}
+
+The D6 rolled a ${Math.floor(Math.random() * 6) + 1}
+
+The D20 rolled a ${Math.floor(Math.random() * 20) + 1}
 `
-        );          
+        );
     }
 );
 
