5cf55133a2c6057d08bffd867cd7fe46f8821259
rvanovermeiren-matc <rvanovermeiren@madisoncollege.edu>
2026-02-03 11:08:42 -0600

    Added hostname to output...

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const http = require('http');
 
+const os = require(
+    'os'
+);
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end(`
 Hello, Rane\'s World ! ! !
     
-The time on the server is ${new Date().toLocaleString()}
+The time on ${os.hostname()} (server) is ${new Date().toLocaleString()}
 
 The D6 rolled a ${Math.floor(Math.random() * 6) + 1}
 The D20 rolled a ${Math.floor(Math.random() * 20) + 1}
