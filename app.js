62e034e232f343656be265150227ab59de7ffcb5
tsteelematc <tsteele@madisoncollege.edu>
2026-02-03 11:08:36 -0600

    Added hostname to output...

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const http = require(
     'http'
 );
 
+const os = require(
+    'os'
+);
+
 const server = http.createServer(
     (req, res) => {
         res.writeHead(
@@ -12,7 +16,7 @@ const server = http.createServer(
         );
         res.end(`Hello, Tom's World ! ! !
 
-The time on the server is ${new Date().toLocaleString()}
+The time on ${os.hostname()} (server) is ${new Date().toLocaleString()}
 
 The D6 rolled a ${Math.floor(Math.random() * 6) + 1}
 
@@ -22,6 +26,7 @@ The D20 rolled a ${Math.floor(Math.random() * 20) + 1}
     }
 );
 
+// Oh yeah, this is where it reads the .env file...
 const port = process.env.PORT || 8080; 
 
 server.listen(
