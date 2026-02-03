e4239ca0d6d84ff94bd3041182acb775c8be240c
bpaulisse <bpaulisse@madisoncollege.edu>
2026-02-03 11:47:27 -0600

    Changed content type to text/html to add HTML text. Tried adding an image, but failed to do so.

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const http = require('http');
 const os = require('os');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end(
-`Hello, Brock's World!
-Hostname: ${os.hostname()}
-Server Time: ${new Date().toLocaleString()}
-The D6 rolled a ${Math.floor( Math.random() * 6 ) + 1}
-The D20 rolled a ${Math.floor( Math.random() * 20 ) + 1}`
+`<html>
+<h1>Hello, Brock's World!</h1>
+<p><b>Hostname</b>: ${os.hostname()}<br>
+<b>Server Time</b>: ${new Date().toLocaleString()}</p>
+
+<p>The <b>D6</b> rolled a <b>${Math.floor( Math.random() * 6 ) + 1}</b><br>
+The <b>D20</b> rolled a <b>${Math.floor( Math.random() * 20 ) + 1}</b></p>
+
+<p>This page uses the content type of '<b>text/html</b>' instead of '<b>text/plain</b>' which allows for this HTML text here.</p>
+</html>`
     );
 });
 
