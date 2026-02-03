5c3044dfab00041b516987998b669ada76db11ab
jarengomatc <jarengo@madisoncollege.edu>
2026-02-03 11:08:39 -0600

    Added hostname to output...

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const http = require('http');
 
+const os = require('os');
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end(`Hello, John's World ! ! !
 
-The time on the server is ${new Date().toLocaleString()}
+The time on ${os.hostname()} (server) is ${new Date().toLocaleString()}
 
 The D6 rolled a ${Math.floor(Math.random() * 6) + 1}
 
 The D20 rolled a ${Math.floor(Math.random() * 20) + 1}`);
 });
 
+// Oh yeah, this is where we set the port!
 const port = process.env.PORT || 8080; 
 server.listen(port,() => {
   console.log(`Listening on port ${port}`);
