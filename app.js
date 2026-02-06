d11908d7072a6c032205c363d4f745e449180851
Peter Barbiaux <PBarbiaux@madisoncollege.edu>
2026-02-06 12:37:51 -0600

    adding another roll for week 3 sharing, this time a d100 for Call of Cthulhu

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const server = http.createServer((req, res) => {
   res.end(`Hello, Peter's World! $!$!$!\n
     The current time on ${os.hostname} is: ${new Date().toLocaleString()}
     The d20 rolled a ${Math.floor(Math.random() * 20) + 1}
-    the d14 rolled a ${Math.floor(Math.random() * 14) + 1}`);          
+    The d14 rolled a ${Math.floor(Math.random() * 14) + 1}
+    The d100 rolled a ${Math.floor(Math.random() * 100) + 1}`);          
 });
 
 const port = process.env.PORT || 8080; 
