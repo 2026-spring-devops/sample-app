def19380447993b26b703e409be08db27e0bb0e4
deecolg <drieberutlin@madisoncollege.edu>
2026-02-10 11:11:29 -0600

    Fixed indentation - shorted code

--- app.js
+++ app.js
@@ -1,43 +1,26 @@
-const http = require(
-    'http'
-);
+const http = require('http');
+const os = require('os');
 
-const os = require(
-    'os'
-);
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hello, Devon's World!</title>
+  </head>
+  <body>
+    <h1>Welcome to my Webpage!</h1>
+    <p>The time on ${os.hostname()} (server) is ${new Date().toLocaleString()}</p>
+    <p>The D2 rolled a ${Math.floor(Math.random() * 2) + 1}</p>
+    <p>The D6 rolled a ${Math.floor(Math.random() * 6) + 1}</p>
+    <p>The D20 rolled a ${Math.floor(Math.random() * 20) + 1}</p>
+  </body>
+</html>`);
+});
 
-const server = http.createServer(
-    (req, res) => {
-        res.writeHead(
-            200, 
-                { 
-                    'Content-Type': 'text/html' 
-                }
-            );
-        res.end(
-            `<html> 
-            <title> Hello, Devons' World ! ! ! </title>
-            
-<h1> Welcome to my Webpage! </h1>
+const port = process.env.PORT || 8080;
 
-<p> The time on ${os.hostname()} (server) is ${new Date().toLocaleString()} </p>
-
-<p> The D2 rolled a ${Math.floor(Math.random() * 2) + 1} </p>
-
-<p> The D6 rolled a ${Math.floor(Math.random() * 6) + 1} </p>
-
-<p> The D20 rolled a ${Math.floor(Math.random() * 20) + 1} </p>
-</html>
-            `
-        );
-    }
-);
-
-const port = process.env.PORT || 8080; 
-
-server.listen(
-    port,
-    () => {
-        console.log(`Listening on port ${port}`);
-    }
-);
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
