26b303ccccbb67e5054ff4d413c6459d790a709b
Scott Bennett <sjbennett2@madisoncollege.edu>
2026-02-22 03:29:07 +0000

    Update deploy-ec2-instance.sh to include timestamp in security group and instance name tags

--- bash/deploy-ec2-instance.sh
+++ bash/deploy-ec2-instance.sh
@@ -3,10 +3,11 @@
 set -e
 
 export AWS_DEFAULT_REGION="us-east-1"
+ts=$(date -u +"%Y%m%d%H%M%S")
 user_data=$(cat user-data.sh)
 
 security_group_id=$(aws ec2 create-security-group \
-  --group-name "sample-app" \
+  --group-name "sample-app-$ts" \
   --description "Allow HTTP traffic into the sample app" \
   --output text \
   --query GroupId)
@@ -28,7 +29,7 @@ instance_id=$(aws ec2 run-instances \
   --instance-type "t2.micro" \
   --security-group-ids "$security_group_id" \
   --user-data "$user_data" \
-  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=sample-app}]' \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=sample-app-$ts}]" \
   --output text \
   --query Instances[0].InstanceId)
 
@@ -40,3 +41,4 @@ public_ip=$(aws ec2 describe-instances \
 echo "Instance ID = $instance_id"
 echo "Security Group ID = $security_group_id"
 echo "Public IP = $public_ip"
+echo "Name tag = sample-app-$ts"
