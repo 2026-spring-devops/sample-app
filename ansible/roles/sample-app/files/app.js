8a792f35b56150775e5ad326c0f8b53cb6b70a7c
tsteelematc <tsteele@madisoncollege.edu>
2026-02-24 09:53:16 -0600

    week-06 prep, placeholder folders etc...

---
+++ ansible/roles/sample-app/files/app.js
@@ -0,0 +1,41 @@
+const http = require(
+    'http'
+);
+
+const os = require(
+    'os'
+);
+
+const server = http.createServer(
+    (req, res) => {
+        res.writeHead(
+            200, 
+            { 
+                'Content-Type': 'text/plain' 
+            }
+        );
+        res.end(`Hello, DevOp's Ansible World ! ! !
+
+The time on ${os.hostname()} (server) is ${new Date().toLocaleString()}
+
+The D6 rolled a ${Math.floor(Math.random() * 6) + 1}
+
+The D20 rolled a ${Math.floor(Math.random() * 20) + 1}
+
+The D4 rolled a ${Math.floor(Math.random() * 4) + 1}
+
+The coin toss was ${(Math.floor(Math.random() * 2) + 1) == 1 ? 'HEADS' : 'TAILS'}
+`
+        );          
+    }
+);
+
+// Oh yeah, this is where it reads the .env file...
+const port = process.env.PORT || 8080; 
+
+server.listen(
+    port,
+    () => {
+        console.log(`Listening on port ${port}`);
+    }
+);
