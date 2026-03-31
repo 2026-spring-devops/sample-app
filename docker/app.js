910d09d436dc6c1fd3f65ba58717d78062c203b0
Jay-Byrd Mystic <jmystic@madisoncollege.edu>
2026-03-31 11:02:30 -0500

    commit

--- docker/app.js
+++ docker/app.js
@@ -14,7 +14,7 @@ const server = http.createServer(
                 'Content-Type': 'text/plain' 
             }
         );
-        res.end(`Hello, DevOp's Docker World ! ! !
+        res.end(`Hello, DevOp's Docker World ! ! ! (Sha-Bang)
 
 The time on ${os.hostname()} (server) is ${new Date().toLocaleString()}
 
